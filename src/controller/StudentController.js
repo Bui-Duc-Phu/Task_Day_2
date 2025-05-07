01c7c843d9e249aa663fdf6e61c6b4bc8c1b3d20
Bui-Duc-Phu <buiphu05012002@gmail.com>
2025-05-07 11:17:18 +0700

    Search by name and add student

--- src/controller/StudentController.js
+++ src/controller/StudentController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Student = require('../model/Student');
+const readline = require('readline-sync');
 
 const studentsFilePath = path.join(__dirname, '../data', 'data.json');
 
@@ -18,15 +19,46 @@ if (fs.existsSync(studentsFilePath)) {
     }
 }
 
+function isMSSVExists(mssv) {
+    return students.some(student => student.MSSV === mssv);
+}
+
+
 function addStudent(student) {
     if (!(student instanceof Student)) {
         throw new Error('Input must be an instance of Student');
     }
-    students.push(student);
+    students.push({
+        MSSV: student.MSSV,
+        age: student.age,
+        averageScore: student.averageScore,
+        name: student.name
+    })
     fs.writeFileSync(studentsFilePath, JSON.stringify(students, null, 2), 'utf8');
 
     console.log(`Student ${student.name} added successfully`);
     return students;
 }
 
-module.exports = { students, addStudent };
+
+const promptStudentData = () => {
+    let mssv;
+    while (true) {
+        mssv = readline.question("Enter student MSSV: ").toUpperCase();
+        if (isMSSVExists(mssv)) {
+            console.log(`MSSV "${mssv}" đã tồn tại. Vui lòng nhập lại.`);
+        } else {
+            break;
+        }
+    }
+    const age = readline.questionInt("Enter student age: ");
+    const averageScore = readline.questionFloat("Enter student average score: ");
+    const name = readline.question("Enter student name: ");
+
+    return new Student(mssv, age, averageScore, name);
+};
+
+
+
+
+module.exports = { students, addStudent,promptStudentData };
