2a474be4f125e3624cb771ed6b5d007dbba0e736
Bui-Duc-Phu <buiphu05012002@gmail.com>
2025-05-07 10:38:04 +0700

    fix

--- src/controller/StudentController.js
+++ src/controller/StudentController.js
@@ -8,15 +8,12 @@ let students = [];
 
 if (fs.existsSync(studentsFilePath)) {
     const rawData = fs.readFileSync(studentsFilePath, 'utf8');
-
-    // Kiểm tra xem dữ liệu có hợp lệ không
     try {
         if (rawData.trim()) {
             students = JSON.parse(rawData);
         }
     } catch (err) {
-        console.error('❌ Lỗi đọc file data.json:', err.message);
-        // Nếu lỗi, đặt lại danh sách sinh viên rỗng
+        console.error('Lỗi đọc file data.json:', err.message);
         students = [];
     }
 }
@@ -25,13 +22,10 @@ function addStudent(student) {
     if (!(student instanceof Student)) {
         throw new Error('Input must be an instance of Student');
     }
-
     students.push(student);
-
-    // Ghi lại vào file JSON
     fs.writeFileSync(studentsFilePath, JSON.stringify(students, null, 2), 'utf8');
 
-    console.log(`✅ Student ${student.name} added successfully`);
+    console.log(`Student ${student.name} added successfully`);
     return students;
 }
 
--- src/index.js
+++ src/index.js
@@ -14,10 +14,22 @@ const addStudent = () => {
 
 const GetData = () => studentController.students
 
+const searchStudentByName = (name) => {
+    const student = studentController.students.find(student => student.name === name);
+    if (student) {
+        return student; 
+    } else {
+        return null;
+    }
+};
+
+
 
 while (true) {
     console.log("\n==== MENU ====");
     console.log("1. add student");
+    console.log("2. get data");
+    console.log("3. Search student by name");
     console.log("0. Exit");
     const choice = readline.question("Your choice: ");
 
@@ -30,6 +42,15 @@ while (true) {
             break;
         case "2":
             console.log(GetData())
+            break;
+        case "3":
+            const name = readline.question("Enter student name to search: ");
+            const student = searchStudentByName(name);
+            if (student) {
+                console.log(`Found student: ${JSON.stringify(student)}`);
+            } else {
+                console.log("Student not found");
+            }
             break;
         default:
             console.log("Invalid choice");
